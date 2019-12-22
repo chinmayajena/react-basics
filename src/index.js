38ee9e7a7018bea4197a5c4d1e3256671cdf0c32
chinmaya.jena@maersk.com <maersk-mtc17@Maersk-MTC17s-MacBook-Pro.local>
2019-12-22 21:16:05 +0530

    added react router and pages

--- src/index.js
+++ src/index.js
@@ -3,8 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Users = () => <h3>I am User!</h3>;
+const Contact = () => <h3>I am contact!</h3>
+
+const routing = (
+    <Router>
+      <div>
+        <Route path="/" component={App} />
+        <Route path="/users" component={Users} />
+        <Route path="/contact" component={Contact} />
+      </div>
+    </Router>
+  )
+  ReactDOM.render(routing, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
